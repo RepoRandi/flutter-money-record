d23d128e31d44f78f8cfc4a61b0851ae97b83cce
Randi <randimaulanaakbar@gmail.com>
2023-01-15 19:17:21 +0700

    fix: update ip

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static const baseUrl = 'http://103.161.76.34/api_money_record';
+  static const baseUrl = 'http://192.168.2.4/api_money_record';
 
   static const history = '$baseUrl/history/';
   static const user = '$baseUrl/user/';
