705a4b4c616cd31fe093d57cfca532c8bd8fe7a1
Randi <randimaulanaakbar@gmail.com>
2023-01-15 21:38:16 +0700

    feat: add home page ui

--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -1,12 +1,32 @@
+import 'package:d_info/d_info.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:money_record/config/session.dart';
+import 'package:money_record/presentation/page/auth/login_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                Session.clearUser();
+                bool? yes = await DInfo.dialogConfirmation(
+                    context, 'Logout', 'You sure logout from this account');
+                if (yes ?? false) {
+                  DInfo.toastSuccess('Logout success');
+                  Get.off(() => const LoginPage());
+                }
+              },
+              icon: const Icon(Icons.logout))
+        ],
+      ),
+      body: const Center(
         child: Text('Home Page'),
       ),
     );
