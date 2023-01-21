7606eb478b3eebfc29f169533969b58dc87b33e0
Randi <randimaulanaakbar@gmail.com>
2023-01-21 13:35:04 +0700

    feat: add color

--- lib/config/app_color.dart
+++ lib/config/app_color.dart
@@ -5,4 +5,7 @@ class AppColor {
   static const secondary = Color(0xFFB9FFFC);
   static const bg = Color(0xFF9AB3F5);
   static const chart = Color(0xFFA3D8F4);
+  static const white = Color(0xFFffffff);
+  static const green = Color(0xFF03c03c);
+  static const red = Color(0xFFff0000);
 }
