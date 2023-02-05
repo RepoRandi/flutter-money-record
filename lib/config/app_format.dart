5c7bc4a5ec1befa84c6e2ebddeca31559c2aaf88
Randi <randimaulanaakbar@gmail.com>
2023-02-05 10:38:42 +0700

    fix: add coma

--- lib/config/app_format.dart
+++ lib/config/app_format.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 class AppFormat {
   static String date(String stringDate) {
     DateTime dateTime = DateTime.parse(stringDate);
-    return DateFormat('d MMM yyyy' 'id_ID').format(dateTime);
+    return DateFormat('d MMM yyyy', 'id_ID').format(dateTime);
   }
 
   static String currency(String number) {
