e64b36d8bef04692283676283bfa00748b9377b4
Randi <randimaulanaakbar@gmail.com>
2023-02-09 17:18:16 +0700

    feat: add controller history

---
+++ lib/presentation/controller/history/c_history.dart
@@ -0,0 +1,37 @@
+import 'package:get/get.dart';
+import 'package:money_record/data/model/history.dart';
+import 'package:money_record/data/source/source_history.dart';
+
+class CHistory extends GetxController {
+  final _loading = false.obs;
+  bool get loading => _loading.value;
+
+  final _list = <History>[].obs;
+  List<History> get list => _list.value;
+
+  getList(idUser) async {
+    _loading.value = true;
+    update();
+
+    _list.value = await SourceHistory.getHistory(idUser);
+    update();
+
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      _loading.value = false;
+      update();
+    });
+  }
+
+  search(idUser, date) async {
+    _loading.value = true;
+    update();
+
+    _list.value = await SourceHistory.getHistorySearch(idUser, date);
+    update();
+
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      _loading.value = false;
+      update();
+    });
+  }
+}
